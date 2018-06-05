de9d2cba35e58d4c3ec80e93c538273470b97190
Álex Puchades <alex94puchades@gmail.com>
2018-06-05 21:32:24 +0200

    Allow unbounded local buffer instances

--- include/peplus/local_buffer.hpp
+++ include/peplus/local_buffer.hpp
@@ -3,6 +3,8 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <cstdint>
+#include <limits>
 
 namespace peplus {
 
@@ -12,7 +14,8 @@ public:
 	template <std::size_t N>
 	constexpr LocalBuffer(const char (&buffer)[N]);
 
-	constexpr LocalBuffer(const char * mem, std::size_t size);
+	constexpr LocalBuffer(const void * mem);
+	constexpr LocalBuffer(const void * mem, std::size_t size);
 
 	constexpr const char * data() const;
 	constexpr std::size_t  size() const;
@@ -39,8 +42,14 @@ template <std::size_t N>
 constexpr LocalBuffer::LocalBuffer(const char (&buffer)[N])
 	: LocalBuffer { buffer, N } {}
 
-constexpr LocalBuffer::LocalBuffer(const char * mem, std::size_t size)
-	: _mem { mem }, _size { size } {}
+constexpr LocalBuffer::LocalBuffer(const void * mem)
+	: LocalBuffer {
+		mem, std::numeric_limits<std::uintptr_t>::max() -
+		     reinterpret_cast<std::uintptr_t>(mem)
+	} {}
+
+constexpr LocalBuffer::LocalBuffer(const void * mem, std::size_t size)
+	: _mem { static_cast<const char *>(mem) }, _size { size } {}
 
 constexpr const char * LocalBuffer::data() const
 {
